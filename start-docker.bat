10cddb821916d8f40ce82d0a7da2274bb34a212a
dbfv <quanbfclan@gmail.com>
2025-07-15 23:15:16 +0700

    updated docker files

--- start-docker.bat
+++ start-docker.bat
@@ -1,4 +1,11 @@
-echo off
-REM Start both client and server using Docker Compose
+@echo off
+echo Starting Financial Management App with Docker...
+echo.
+echo This will start both the server (port 5000) and client (port 3000)
+echo.
+echo Building and starting containers...
 docker-compose up --build
-pause 
+echo.
+echo If you want to run in background, use: docker-compose up -d --build
+echo To stop containers: docker-compose down
+pause
